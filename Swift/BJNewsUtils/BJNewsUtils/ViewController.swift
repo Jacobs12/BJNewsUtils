d45e6734ab70e39a9031b0380c42e322393e8bfb
fengtao92 <1440913385@qq.com>
2023-06-06 16:52:58 +0800

    commit

---
+++ Swift/BJNewsUtils/BJNewsUtils/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BJNewsUtils
+//
+//  Created by wolffy on 2023/6/6.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
